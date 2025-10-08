247bcc8e08aabdd91af068316ad8ab7e8b6d0f24
Kacper <kacdowe30@gmail.com>
2025-10-08 10:30:04 +0200

    Initial commit

---
+++ src/FileItem.jsx
@@ -0,0 +1,10 @@
+function FileItem() {
+return(
+    <div className="item">
+        <p>Nazwa pliku</p>
+        <p>Plik tekstowy</p>
+        <p>1024mb</p>
+    </div>
+)
+}
+export default FileItem
